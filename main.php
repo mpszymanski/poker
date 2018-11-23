c5a5707c36a4f06a0f719623bfd6af1a70c785a6
Szyman <szyman040395@gmail.com>
2018-11-23 17:31:29 +0100

    Analyse view with stats

--- main.php
+++ main.php
@@ -8,13 +8,20 @@
 
 include 'loader.php';
 
+$start_time = microtime(TRUE);
+
 $builder = new DealBuilder();
 $analyzer = new PokerAnalyzer();
 
-for ($i = 0; $i < 1000; $i++) {
+for ($i = 0; $i < 100000; $i++) {
     $deal = $builder->buildRandomDeal();
     $analyzer->AnalyzeDeal($deal);
 }
 
 $view = new AnalyzeView($analyzer);
 $view->render();
+
+$end_time = microtime(TRUE);
+
+$time = round(($end_time - $start_time) * 1000);
+echo "Calculate in {$time}ms\n";
--- src/views/AnalyzeView.php
+++ src/views/AnalyzeView.php
@@ -10,6 +10,19 @@ class AnalyzeView
 {
     private $analyzer;
 
+    private $expectedResults = [
+        'High card' => 50.118,
+        'Pair' => 42.257,
+        'Two Pair' => 4.754,
+        'Three of a kind' => 2.113,
+        'Straight' => 0.392,
+        'Flush' => 0.197,
+        'Full House' => 0.144,
+        'Quads' => 0.024,
+        'Straight Flush' => 0.001385,
+        'Royal Flush' => 0.000154,
+    ];
+
     public function __construct($analyzer)
     {
         $this->setAnalyzer($analyzer);
@@ -24,10 +37,20 @@ class AnalyzeView
     {
         $analyzer = $this->analyzer;
         $count = $analyzer->getDealsCount();
-        foreach ($analyzer->getHandsCounts() as $handName => $handsCount) {
-            $partOfTheRandom = $handsCount / $count * 100;
-            echo "==$handName==\n";
-            echo "Randomized: $handsCount ($partOfTheRandom%)\t \n";
+        $handsCount = $analyzer->getHandsCounts();
+
+        echo "Hand\t\t\t|\tRandomized\t|\tExpected\t|\n";
+
+        echo "-------------------------------------------------------------------------\n";
+
+        foreach ($handsCount as $handName => $handCount) {
+            $partOfTheRandom = $handCount / $count * 100;
+            $expectedPartOfTheRandom = $this->expectedResults[$handName];
+            $expected = round($count * $expectedPartOfTheRandom / 100);
+            if (strlen($handName) < 6) $handName .= "\t";
+            echo "$handName\t\t|\t$handCount ($partOfTheRandom%)\t|\t$expected ($expectedPartOfTheRandom%)\t|\n";
         }
+
+        echo "\n";
     }
 }
